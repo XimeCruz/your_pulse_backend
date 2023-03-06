dc9f41cfbe0163615df7a4ee67fa3be579aa1862
XimeCruz <xmenacruzt@gmail.com>
2023-03-06 15:33:39 -0400

    first commit

---
+++ src/main/java/proyect/your_pulse_backend/YourPulseBackendApplication.java
@@ -0,0 +1,13 @@
+package proyect.your_pulse_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourPulseBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YourPulseBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/proyect/your_pulse_backend/YourPulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package proyect.your_pulse_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YourPulseBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
